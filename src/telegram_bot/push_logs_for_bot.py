047ef9860329e7f6b964777168f5637b0e05410b
david <daveaneo@gmail.com>
2023-07-21 16:57:44 -0600

    moved env fetch to inside function.

--- src/telegram_bot/push_logs_for_bot.py
+++ src/telegram_bot/push_logs_for_bot.py
@@ -20,19 +20,6 @@ hostname = socket.gethostname()
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 # load_dotenv()
 
-security_log_file = os.getenv('SECURITY_LOGFILE') if os.getenv('SECURITY_LOGFILE') else "telegram_bot_security_file"
-general_log_file = os.getenv('GENERAL_LOGFILE') if os.getenv('GENERAL_LOGFILE') else "telegram_bot_security_file"
-rsa_id_path = os.getenv('RSA_ID_PATH')
-remote_directory_path = os.getenv('REMOTE_PATH')  # user@host:path_to_directory
-
-# Check if all necessary environment variables are set
-required_vars = ['SECURITY_LOGFILE', 'GENERAL_LOGFILE', 'RSA_ID_PATH', 'REMOTE_PATH']
-missing_vars = [var for var in required_vars if os.getenv(var) is None]
-
-if missing_vars:
-    missing_vars_str = ', '.join(missing_vars)
-    log_to_bot(f"Error: The following environment variables are not set: {missing_vars_str}")
-    raise Exception(f"Missing environment variables: {missing_vars_str}")
 
 
 def run(cmd: str) -> None:
@@ -50,6 +37,20 @@ def run(cmd: str) -> None:
 
 
 def push_logs():
+    security_log_file = os.getenv('SECURITY_LOGFILE') if os.getenv('SECURITY_LOGFILE') else "telegram_bot_security_file"
+    general_log_file = os.getenv('GENERAL_LOGFILE') if os.getenv('GENERAL_LOGFILE') else "telegram_bot_security_file"
+    rsa_id_path = os.getenv('RSA_ID_PATH')
+    remote_directory_path = os.getenv('REMOTE_PATH')  # user@host:path_to_directory
+
+    # Check if all necessary environment variables are set
+    required_vars = ['SECURITY_LOGFILE', 'GENERAL_LOGFILE', 'RSA_ID_PATH', 'REMOTE_PATH']
+    missing_vars = [var for var in required_vars if os.getenv(var) is None]
+
+    if missing_vars:
+        missing_vars_str = ', '.join(missing_vars)
+        log_to_bot(f"Error: The following environment variables are not set: {missing_vars_str}")
+        raise Exception(f"Missing environment variables: {missing_vars_str}")
+
     name_to_file = {
         "health_logs": security_log_file,
         "general_logs": general_log_file
