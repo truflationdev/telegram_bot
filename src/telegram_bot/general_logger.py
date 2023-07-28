4db4e90b85eff96e0e8bb318d7eb881b1f185501
david <daveaneo@gmail.com>
2023-07-28 09:13:09 -0600

    updated log to bot general to take strings or dictionaries

--- src/telegram_bot/general_logger.py
+++ src/telegram_bot/general_logger.py
@@ -21,10 +21,6 @@ from telegram_bot.utilities import delete_entries_older_than_x_days, save_logs, 
 import telegram_bot.utilities
 from typing import Union, Dict
 
-# load_dotenv()
-# log_file_path = os.getenv('GENERAL_LOGFILE') if os.getenv('GENERAL_LOGFILE') else "telegram_bot_general_logfile"
-# log_life = int(os.getenv('LOG_LIFE') if os.getenv('LOG_LIFE') else 3)
-
 
 # todo -- create push through that doesn't rely on saving/loading logs
 
@@ -35,7 +31,7 @@ def log_to_bot(input_data: Union[Dict, str]) -> None:
     :param input_data: the data to process, which can be a dictionary, a JSON string, or a non-JSON string
     """
     log_file_path = os.getenv('GENERAL_LOGFILE') if os.getenv('GENERAL_LOGFILE') else "telegram_bot_general_logfile"
-    telegram_bot.utilities.log_to_bot(my_json_arg, log_file_path)
+    telegram_bot.utilities.log_to_bot(input_data, log_file_path)
 
 
 if __name__ == '__main__':
